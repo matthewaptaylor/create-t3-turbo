8fa5dc11178a3a58a253d45c334077bef25b158c
Matthew Taylor <matthewaptaylor@gmail.com>
2024-03-02 13:18:35 +1300

    feat: add environment variable validation

--- apps/expo/src/env.ts
+++ apps/expo/src/env.ts
@@ -2,13 +2,15 @@ import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
 
 export const env = createEnv({
-  clientPrefix: "VITE_",
+  clientPrefix: "EXPO_PUBLIC_",
   /**
    * Specify your client-side environment variables schema here.
    * For them to be exposed to the client, prefix them with `VITE_`.
    */
   client: {
-    VITE_TITLE: z.string().min(1),
+    EXPO_PUBLIC_TITLE: z.string().min(1),
   },
-  runtimeEnv: import.meta.env,
+  runtimeEnv: {
+    EXPO_PUBLIC_TITLE: process.env.EXPO_PUBLIC_TITLE,
+  },
 });
